008db877d6e7b56a33aca04d12dfef22bb67226e
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-09-02 01:29:29 +0300

    Задание лайт 1

--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,3 +1,70 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class EmployeeBook {
+    static int counter =1;
+
+    String employee;
+    int department;
+    int salary;
+    int id;
+
+    public EmployeeBook(String employee, int department, int salary) {
+        this.employee = employee;
+        this.department = department;
+        this.salary = salary;
+        id = counter;
+        counter++;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " +employee + " " + department +" "+ salary;
+    }
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeBook that = (EmployeeBook) o;
+        return department == that.department && salary == that.salary && id == that.id && employee.equalsIgnoreCase(that.employee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employee, department, salary, id);
+    }
+//private String[] employees = {};
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание лайт 1");
+        taskLiteOne();
+
+        System.out.println("Задание Лайт 2");
+        taskLiteTwo();
 
     }
+
+    private static void taskLiteOne() {
+        EmployeeBook[] employees = new EmployeeBook[10];
+        addEmployeesToArray(employees);
+        int sumSalary = 0;
+        int maxSalary = 0;
+        String employeeMaxSalary = "";
+        int minSalary = employees[0].getSalary();
+        String employeeMinSalary = employees[0].getEmployee();
+
+
+        System.out.println("Список сотрудников:");
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+            sumSalary = sumSalary +  employees[i].getSalary();
+        }
+
+        System.out.println("Сумма затрат в месяц: "+sumSalary);
+
+        for (int i = 0; i < employees.length; i++) {
+            if(maxSalary < employees[i].getSalary()){
+                maxSalary = employees[i].getSalary();
+                employeeMaxSalary = employees[i].getEmployee();
+            } else if (minSalary > employees[i].getSalary()) {
+                minSalary = employees[i].getSalary();
+                employeeMinSalary = employees[i].getEmployee();
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой - "+ employeeMaxSalary + " - " +maxSalary +
+                "\nСотрудник с минимальной зарплатой - "+ employeeMinSalary + " - " +minSalary);
+
+        int avgSalary = sumSalary / employees.length;
+        System.out.println("Средняя зарплата - " + avgSalary);
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i].getEmployee());
+        }
+    }
+
+    private static void taskLiteTwo() {
+
+
+    }
+
+    public static void addEmployeesToArray(Object[] arr) {
+        int salary=100_000;
+        for (int i = 0; i < arr.length; i++) {
+            EmployeeBook employee = new EmployeeBook("Иванов Иван Иванович"+i, 1+i, salary);
+            arr[i] = employee;
+            salary += salary;
+            //System.out.println(employee);
+        }
+    }
+
+
 }
