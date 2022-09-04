4f5ba9fb9569cb33cb1aa0aa358197e45457b3f8
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-09-04 14:23:28 +0300

    Задание лайт 1 вариант 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание лайт 1");
@@ -41,7 +39,7 @@ public class Main {
         System.out.println("Сотрудник с максимальной зарплатой - "+ employeeMaxSalary + " - " +maxSalary +
                 "\nСотрудник с минимальной зарплатой - "+ employeeMinSalary + " - " +minSalary);
 
-        int avgSalary = sumSalary / employees.length;
+        float avgSalary = (float) sumSalary / employees.length;
         System.out.println("Средняя зарплата - " + avgSalary);
 
         for (int i = 0; i < employees.length; i++) {
@@ -50,17 +48,81 @@ public class Main {
     }
 
     private static void taskLiteTwo() {
+        int count = 10;
+        EmployeeBook[] employees = new EmployeeBook[count];
+        addEmployeesToArray(employees);
+        String[] name = new String[count];
+        int[] salary = new int[count];
+
+        for (int i = 0; i < employees.length; i++) {
+            name[i] = employees[i].getEmployee();
+            salary[i] = employees[i].getSalary();
+        }
+        int totalSalary = sumSalary(salary);
+        System.out.println("Сумма затрат в месяц: "+totalSalary);
+        int maxSalary = maxSalary(salary);
+        int minSalary = minSalary(salary);
+        String employeeMaxSalary = employeeAnySalary(employees, maxSalary);
+        String employeeMinSalary = employeeAnySalary(employees, minSalary);
+        float avgSalary = (float) totalSalary/employees.length;
 
 
+
+        System.out.println ("Сотрудник с максимальной зарплатой - "+employeeMaxSalary+   " - " + maxSalary +
+                "\nСотрудник с минимальной зарплатой - "+ employeeMinSalary + " - " +minSalary);
+
+        System.out.println("Средняя зарплата - " + avgSalary);
     }
+
+
+    private static String employeeAnySalary(EmployeeBook[] arr, int maxSalary) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getSalary() == maxSalary) {
+                return arr[i].getEmployee();
+            }
+        }
+        return "";
+    }
+
+    private static int maxSalary(int[] arr) {
+        int element = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (element < arr[i]) {
+                element = arr[i];
+            }
+        }
+        return element;
+    }
+
+    private static int minSalary(int[] arr) {
+        int element = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (element > arr[i]) {
+                element = arr[i];
+            }
+        }
+        return element;
+    }
+
+
+    private static int sumSalary(int[] arr) {
+       int slr = 0;
+        for (int i = 0; i < arr.length; i++) {
+            slr = slr + arr[i];
+        }
+        return slr;
+    }
+
+
+
 
     public static void addEmployeesToArray(Object[] arr) {
         int salary=100_000;
         for (int i = 0; i < arr.length; i++) {
-            EmployeeBook employee = new EmployeeBook("Иванов Иван Иванович"+i, 1+i, salary);
+            EmployeeBook employee = new EmployeeBook("Петров Петр Петрович"+i, 1+i, salary);
             arr[i] = employee;
             salary += salary;
-            //System.out.println(employee);
+            System.out.println(employee);
         }
     }
 
