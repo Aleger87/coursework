6736cb91a2afca722096fc5b38c19270c89913a1
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-09-09 01:13:12 +0300

    Задание Хард (исправление замечаний)

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,21 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
        Employee[] employees = new Employee[10];
-       EmployeeBook employeeBook  = new EmployeeBook();
-       employeeBook.addEmployeesToArray(employees);
-       employeeBook.getStartTaskOne(employees);
-        employeeBook.getStartTaskTwo(employees);
+       EmployeeBook.addEmployeesToArray(employees);
+       EmployeeBook.getStartTaskOne(employees);
+       EmployeeBook.getStartTaskTwo(employees);
        System.out.println("\nДобавление изменение сотрудников\n");
        int id = 6;
-       employeeBook.deleteEmployee(employees,id); //удаление сотрудника по id
+       EmployeeBook.deleteEmployee(employees,id); //удаление сотрудника по id
 
        System.out.println(employees[id-1] + " Сотрудник с id - " + id + " уволен\n");
 
-       employeeBook.addEmployee(employees, "Сидоров Петр Михайлович",1, 100);  // добавление нового сотрудника в пустую ячейку
-       employeeBook.replaceEmployee(employees,"Иванов Иван Иванович", 4, 1_000_000, 2); // изменение данных сотрудника по id
-       for (int i = 0; i < employees.length; i++) {
+       Employee newEmployee = new Employee("Степанов Степан Степанович" , 1, 100);
+       EmployeeBook.addEmployee(employees, newEmployee);  // добавление нового сотрудника в пустую ячейку
+        Employee replaceEmployee = new Employee("Иванов Иван Иванович" , 1, 200);
+        EmployeeBook.replaceEmployee(employees, replaceEmployee, 7); // изменение данных сотрудника по id,
+
+        for (int i = 0; i < employees.length; i++) {
             System.out.println(employees[i]);
         }
 
